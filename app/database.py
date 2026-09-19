a3309e7aff36de9e2498b2388b9d32f58a1fb7dd
Hari369 <hiranmaymondal369@gmail.com>
2026-09-19 09:49:04 +0000

    configured with .env file

--- app/database.py
+++ app/database.py
@@ -23,7 +23,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not configured in .env")
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
